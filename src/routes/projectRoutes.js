349836653803feb5b08493e2c3363eb468f51274
naolt <naoltamrat11@gmai.com>
2023-12-30 17:34:34 +0300

    Implement additional Task Management routes in Project Management
    - Updated projectRoutes to include routes for task management within a project

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -1,24 +1,59 @@
 const express = require("express");
 const router = express.Router();
 const projectController = require("../controllers/projectController");
+const taskController = require("../controllers/taskController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const roleMiddleware = require("../middlewares/roleMiddleware");
 
 // Fetch all projects route
 router.get("/", authMiddleware, projectController.getAllProjects);
 
 // Create a new project route
-router.post("/", authMiddleware, projectController.createProject);
+router.post(
+  "/",
+  authMiddleware,
+  projectController.validateProjectCreation,
+  projectController.createProject
+);
+
+// Fetch all tasks for a specific project route
+router.get(
+  "/:projectId/tasks",
+  authMiddleware,
+  roleMiddleware("admin"),
+  taskController.getAllTasksForProject
+);
+
+// Create a new task within a project route
+router.post(
+  "/:projectId/tasks",
+  authMiddleware,
+  roleMiddleware("admin"),
+  taskController.validateTaskCreation,
+  taskController.createTask
+);
 
 // Fetch a specific project by ID route
-router.get("/:projectId", authMiddleware, projectController.getProjectById);
+router.get(
+  "/:projectId",
+  authMiddleware,
+  roleMiddleware("admin"),
+  projectController.getProjectById
+);
 
 // Update a specific project by ID route
-router.put("/:projectId", authMiddleware, projectController.updateProjectById);
+router.put(
+  "/:projectId",
+  authMiddleware,
+  roleMiddleware("admin"),
+  projectController.updateProjectById
+);
 
 // Delete a specific project by ID route
 router.delete(
   "/:projectId",
   authMiddleware,
+  roleMiddleware("admin"),
   projectController.deleteProjectById
 );
 
