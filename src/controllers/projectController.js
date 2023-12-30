3d9f0a24e464555608d71b708bc19ea35c49d7c3
naolt <naoltamrat11@gmai.com>
2023-12-30 17:44:38 +0300

    Fix authorization issue in projectController
    - Modified projectController to address authorization issues

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -4,12 +4,19 @@ const prisma = new PrismaClient();
 
 async function getAllProjects(req, res) {
   try {
-    // Get all projects for the authenticated user
-    const projects = await prisma.project.findMany({
-      where: { user_id: req.user.id },
-    });
-
-    res.json(projects);
+    // Check if user is an admin and a user_id is provided in the query parameters
+    if (req.user.role === "admin" && req.query.user_id) {
+      const projects = await prisma.project.findMany({
+        where: { user_id: parseInt(req.query.user_id, 10) },
+      });
+      res.json(projects);
+    } else {
+      // Fetch all projects for the authenticated user
+      const projects = await prisma.project.findMany({
+        where: { user_id: req.user.id },
+      });
+      res.json(projects);
+    }
   } catch (error) {
     console.error("Error fetching projects:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -20,16 +27,29 @@ async function createProject(req, res) {
   const { name, description } = req.body;
 
   try {
-    // Create a new project for the authenticated user
-    const project = await prisma.project.create({
-      data: {
-        name,
-        description,
-        user: { connect: { id: req.user.id } },
-      },
-    });
+    // Check if user is an admin and a user_id is provided in the query parameters
+    if (req.user.role === "admin" && req.query.user_id) {
+      const project = await prisma.project.create({
+        data: {
+          name,
+          description,
+          user: { connect: { id: parseInt(req.query.user_id, 10) } },
+        },
+      });
 
-    res.status(201).json(project);
+      res.status(201).json(project);
+    } else {
+      // Create a new project for the authenticated user
+      const project = await prisma.project.create({
+        data: {
+          name,
+          description,
+          user: { connect: { id: req.user.id } },
+        },
+      });
+
+      res.status(201).json(project);
+    }
   } catch (error) {
     console.error("Error creating project:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -40,16 +60,25 @@ async function getProjectById(req, res) {
   const projectId = parseInt(req.params.projectId, 10);
 
   try {
-    // Get a specific project by ID for the authenticated user
-    const project = await prisma.project.findUnique({
-      where: { id: projectId, user_id: req.user.id },
-    });
-
-    if (!project) {
-      return res.status(404).json({ error: "Project not found" });
+    // Check if user is an admin and a user_id is provided in the query parameters
+    if (req.user.role === "admin" && req.query.user_id) {
+      const project = await prisma.project.findUnique({
+        where: { id: projectId, user_id: parseInt(req.query.user_id, 10) },
+      });
+      if (!project) {
+        return res.status(404).json({ error: "Project not found" });
+      }
+      res.json(project);
+    } else {
+      // Get a specific project by ID for the authenticated user
+      const project = await prisma.project.findUnique({
+        where: { id: projectId, user_id: req.user.id },
+      });
+      if (!project) {
+        return res.status(404).json({ error: "Project not found" });
+      }
+      res.json(project);
     }
-
-    res.json(project);
   } catch (error) {
     console.error("Error fetching project by ID:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -61,13 +90,23 @@ async function updateProjectById(req, res) {
   const { name, description } = req.body;
 
   try {
-    // Update a specific project by ID for the authenticated user
-    const updatedProject = await prisma.project.update({
-      where: { id: projectId, user_id: req.user.id },
-      data: { name, description },
-    });
+    // Check if user is an admin and a user_id is provided in the query parameters
+    if (req.user.role === "admin" && req.query.user_id) {
+      const updatedProject = await prisma.project.update({
+        where: { id: projectId, user_id: parseInt(req.query.user_id, 10) },
+        data: { name, description },
+      });
 
-    res.json(updatedProject);
+      res.json(updatedProject);
+    } else {
+      // Update a specific project by ID for the authenticated user
+      const updatedProject = await prisma.project.update({
+        where: { id: projectId, user_id: req.user.id },
+        data: { name, description },
+      });
+
+      res.json(updatedProject);
+    }
   } catch (error) {
     console.error("Error updating project by ID:", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -78,17 +117,37 @@ async function deleteProjectById(req, res) {
   const projectId = parseInt(req.params.projectId, 10);
 
   try {
-    // Delete a specific project by ID for the authenticated user
-    await prisma.project.delete({
-      where: { id: projectId, user_id: req.user.id },
-    });
+    // Check if user is an admin and a user_id is provided in the query parameters
+    if (req.user.role === "admin" && req.query.user_id) {
+      // Delete a specific project by ID for the specified user
+      await prisma.project.delete({
+        where: { id: projectId, user_id: parseInt(req.query.user_id, 10) },
+      });
+    } else {
+      // Delete a specific project by ID for the authenticated user
+      await prisma.project.delete({
+        where: { id: projectId, user_id: req.user.id },
+      });
+    }
 
-    res.json({ message: "Project deleted successfully" });
+    res.status(204).json({ message: "Project deleted successfully" });
   } catch (error) {
     console.error("Error deleting project by ID:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
+
+// Validate project creation data
+const validateProjectCreation = (req, res, next) => {
+  const { name } = req.body;
+
+  // Check if required fields are present
+  if (!name) {
+    return res.status(400).json({ error: "Project name is required" });
+  }
+
+  next();
+};
 
 module.exports = {
   getAllProjects,
@@ -96,4 +155,5 @@ module.exports = {
   getProjectById,
   updateProjectById,
   deleteProjectById,
+  validateProjectCreation,
 };
